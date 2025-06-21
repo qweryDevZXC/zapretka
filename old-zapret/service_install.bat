a3e2720a366a8c0bedb709880546a654c81a9dcd
KataLoved <Zaykamurr@yandex.ru>
2025-06-21 12:42:29 +0300

    init

---
+++ old-zapret/service_install.bat
@@ -0,0 +1,138 @@
+@echo off
+setlocal EnableDelayedExpansion
+chcp 65001 > nul
+:: 65001 - UTF-8
+
+net session >nul 2>&1
+if not %errorLevel% == 0 (
+	powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/c \"\"%~f0\" admin\"' -Verb RunAs"
+	exit /b
+)
+
+:: Main
+cd /d "%~dp0"
+set BIN_PATH=%~dp0bin\
+set LISTS_PATH=%~dp0lists\
+
+set "gameFlagFile=%~dp0bin\game_filter.enabled"
+if exist "%gameFlagFile%" (
+    set "GameFilterStatus=enabled"
+    set "GameFilter=1024-65535"
+) else (
+    set "GameFilterStatus=disabled"
+    set "GameFilter=0"
+)
+
+:: Searching for .bat files in current folder, except files that start with "service"
+echo Pick one of the options:
+set "count=0"
+for %%f in (*.bat) do (
+    set "filename=%%~nxf"
+    if /i not "!filename:~0,7!"=="service" if /i not "!filename:~0,13!"=="check_updates" if /i not "!filename:~0,17!"=="cloudflare_switch" if /i not "!filename!"=="set_to_default.bat" (
+        set /a count+=1
+        echo !count!. %%f
+        set "file!count!=%%f"
+    )
+)
+
+:: Choosing file
+set "choice="
+set /p "choice=Input file index (number): "
+if "!choice!"=="" goto :eof
+
+set "selectedFile=!file%choice%!"
+if not defined selectedFile (
+    echo Wrong choice, exiting...
+    pause
+    goto :eof
+)
+
+:: Parsing args (mergeargs: 2=start param|1=params args|0=default)
+set "args="
+set "capture=0"
+set "mergeargs=0"
+set QUOTE="
+
+for /f "tokens=*" %%a in ('type "!selectedFile!"') do (
+    set "line=%%a"
+
+    echo !line! | findstr /i "%BIN%winws.exe" >nul
+    if not errorlevel 1 (
+        set "capture=1"
+    )
+
+    if !capture!==1 (
+        if not defined args (
+            set "line=!line:*%BIN%winws.exe"=!"
+        )
+
+        set "temp_args="
+        for %%i in (!line!) do (
+            set "arg=%%i"
+
+            if not "!arg!"=="^" (
+                if "!arg:~0,2!" EQU "--" if not !mergeargs!==0 (
+                    set "mergeargs=0"
+                )
+
+                if "!arg:~0,1!" EQU "!QUOTE!" (
+                    set "arg=!arg:~1,-1!"
+
+                    echo !arg! | findstr ":" >nul
+                    if !errorlevel!==0 (
+                        set "arg=\!QUOTE!!arg!\!QUOTE!"
+                    ) else if "!arg:~0,1!"=="@" (
+                        set "arg=\!QUOTE!@%~dp0!arg:~1!\!QUOTE!"
+                    ) else if "!arg:~0,5!"=="%%BIN%%" (
+                        set "arg=\!QUOTE!!BIN_PATH!!arg:~5!\!QUOTE!"
+                    ) else if "!arg:~0,7!"=="%%LISTS%%" (
+                        set "arg=\!QUOTE!!LISTS_PATH!!arg:~7!\!QUOTE!"
+                    ) else (
+                        set "arg=\!QUOTE!%~dp0!arg!\!QUOTE!"
+                    )
+                ) else if "!arg:~0,12!" EQU "%%GameFilter%%" (
+                    set "arg=%GameFilter%"
+                )
+
+                if !mergeargs!==1 (
+                    set "temp_args=!temp_args!,!arg!"
+                ) else if !mergeargs!==3 (
+                    set "temp_args=!temp_args!=!arg!"
+                    set "mergeargs=1"
+                ) else (
+                    set "temp_args=!temp_args! !arg!"
+                )
+
+                if "!arg:~0,2!" EQU "--" (
+                    set "mergeargs=2"
+                ) else if !mergeargs!==2 (
+                    set "mergeargs=1"
+                ) else if !mergeargs!==1 (
+                    for %%x in (!args_with_value!) do (
+                        if /i "%%x"=="!arg!" (
+                            set "mergeargs=3"
+                        )
+                    )
+                )
+            )
+        )
+
+        if not "!temp_args!"=="" (
+            set "args=!args! !temp_args!"
+        )
+    )
+)
+
+:: Creating service with parsed args
+set ARGS=%args%
+echo Final args: !ARGS!
+set SRVCNAME=zapret
+
+net stop %SRVCNAME% >nul 2>&1
+sc delete %SRVCNAME% >nul 2>&1
+sc create %SRVCNAME% binPath= "\"%BIN_PATH%winws.exe\" %ARGS%" DisplayName= "zapret" start= auto
+sc description %SRVCNAME% "Zapret DPI bypass software"
+sc start %SRVCNAME%
+
+pause
+endlocal
---
+++ old-zapret/service_remove.bat
@@ -0,0 +1,22 @@
+@echo off
+chcp 65001 > nul
+:: 65001 - UTF-8
+
+net session >nul 2>&1
+if not %errorLevel% == 0 (
+	powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/c \"\"%~f0\" admin\"' -Verb RunAs"
+	exit /b
+)
+
+set SRVCNAME=zapret
+
+net stop %SRVCNAME%
+sc delete %SRVCNAME%
+
+net stop "WinDivert"
+sc delete "WinDivert"
+net stop "WinDivert14"
+sc delete "WinDivert14"
+
+pause
+endlocal
